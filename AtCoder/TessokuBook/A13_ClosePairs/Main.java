864b0f261ec87ece785c809857da83b61d6ee945
mamemamema <st.msnr.wk@gmail.com>
2025-12-13 14:06:01 +0000

    TessokuBook

---
+++ AtCoder/TessokuBook/A13_ClosePairs/Main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+/*
+  二分探索法を使用して解く問題。
+*/
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int cntNums = sc.nextInt();
+		long diff = sc.nextInt();
+		long[] numsAsc = new long[cntNums];
+		for (int i = 0; i < cntNums; i++) {
+			numsAsc[i] = sc.nextInt();
+		}
+
+	}
+
+}
